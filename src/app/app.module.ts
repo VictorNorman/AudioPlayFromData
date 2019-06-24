aa07fd35b3ae87faa498e65128d15a7e4f47df9d
Victor Norman <vtn2@calvin.edu>
2019-06-24 16:07:11 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,18 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    WebView,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FilesystemDirectory, Filesystem } from '@capacitor/core';
+import { HttpClient } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public datafile = '';
+  private win: any = window;
+
+  constructor(
+    private http: HttpClient,
+    private webView: WebView,
+  ) {}
+
+  copyFile() {
+    this.http.get('assets/1.mp3', {
+      responseType: 'arraybuffer'
+    }).subscribe(data => {
+      Filesystem.writeFile({
+        data: data as unknown as string,
+        path: 'EN-1.mp3',
+        directory: FilesystemDirectory.Data,
+      }).then((result) => {
+        Filesystem.getUri({
+          directory: FilesystemDirectory.Data,
+          path: 'EN-1.mp3',
+        }).then((result2) => {
+          const path = result2.uri;
+          console.log('😁 result2.uri is', path);
+
+          this.datafile = this.webView.convertFileSrc(path);
+
+          // this.datafile = 'assets/1.mp3';
+          console.log('😔 this.datafile is ', this.datafile);
+        });
+      });
+    });
+  }
 
 }
